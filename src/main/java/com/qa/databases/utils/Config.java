9f42cd10e41248b08d27d7356fed03825d5c5f86
Charlie <creddin788@gmail.com>
2020-02-11 21:25:21 +0000

    removed hardcoded passwords

--- src/main/java/com/qa/databases/utils/Config.java
+++ src/main/java/com/qa/databases/utils/Config.java
@@ -2,8 +2,8 @@ package com.qa.databases.utils;
 
 public class Config {
 
-	public static final String username = "Tsar_Charlie";
-	public static final String password = "Skippy101!";
+	public static String username;
+	public static String password;
 	public static final String url = "jdbc:mysql://34.89.101.76:3306/IMS\\";
 			
 	public static String getUsername() {
